7623e48d18467af0322a7030ad8556613094e6ab
AayeshaNakarmi <121529037+AayeshaNakarmi@users.noreply.github.com>
2023-08-26 19:12:04 +0545

    UI

--- app.py
+++ app.py
@@ -1,54 +1,52 @@
 import streamlit as st
-from streamlit_chat import message
-import llama
 
-def clear_chat():
-    st.session_state.messages = [{"role": "assistant", "content": "Say something to get started!"}]
+st.set_page_config(page_title="Generate your Email!!",page_icon=":📬:")
 
 
-st.title("Llama2 Clarifai Tutorial")
+import streamlit as st
 
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Say something to get started!"}]
-
-
-with st.form("chat_input", clear_on_submit=True):
-    a, b = st.columns([4, 1])
-
-    user_prompt = a.text_input(
-        label="Your message:",
-        placeholder="Type something...",
-        label_visibility="collapsed",
-    )
-
-    b.form_submit_button("Send", use_container_width=True)
-
-
-for msg in st.session_state.messages:
-    message(msg["content"], is_user=msg["role"] == "user")
-
-
-if user_prompt:
-
-    print('user_prompt: ', user_prompt)
-
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
+def main():
+    st.title("Email Generator")
     
-    message(user_prompt, is_user=True)
+    # User input for sender's name
+    sender_name = st.text_input("Sender's Name")
+    
+    # User input for recipient's name
+    recipient_name = st.text_input("Recipient's Name")
+    
+    # User input for email subject/topic
+    subject = st.text_input("Subject/Topic")
+    
+    # User input for email tone with dropdown
+    tone_options = ['Formal', 'Casual', 'Friendly']
+    tone = st.selectbox("Tone", tone_options)
+    
+    # User input for attachments
+    attachments = st.file_uploader("Attachments", type=["pdf", "txt", "docx"], accept_multiple_files=True)
+    
+    # Create Email button
+    if st.button("Create Email"):
+        email_content = generate_email(sender_name, recipient_name, subject, tone, attachments)
+        st.write("## Email Preview")
+        st.write(email_content)
 
-    response = llama.get_response(user_prompt)  # get response from llama2 API (in our case from Workflow we created before)
+def generate_email(sender_name, recipient_name, subject, tone, attachments):
+    # Generate the email content based on user inputs
+    email_template = f"Dear {recipient_name},\n\n"
+    
+    if tone == 'Formal':
+        email_template += "I hope this email finds you well. "
+    elif tone == 'Casual':
+        email_template += "Hey there, hope you're doing great! "
+    else:
+        email_template += "Hi, just wanted to drop you a quick note. "
+    
+    email_template += f"I wanted to discuss the topic of '{subject}'. "
+    email_template += "Please find the attached files for your reference.\n\n"
+    
+    email_template += f"Best regards,\n{sender_name}"
+    
+    return email_template
 
-    msg = {"role": "assistant", "content": response}
-
-    print('st.session_state.messages: ', st.session_state.messages)
-
-    st.session_state.messages.append(msg)
-
-    print('msg.content: ', msg["content"])
-
-    message(msg["content"])
-
-
-if len(st.session_state.messages) > 1:
-    st.button('Clear Chat', on_click=clear_chat)
+if __name__ == "__main__":
+    main()
