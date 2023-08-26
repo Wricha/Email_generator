873dd54f50b2c4377cefdfd674b53eb4b8de71e8
AayeshaNakarmi <121529037+AayeshaNakarmi@users.noreply.github.com>
2023-08-26 20:57:19 +0545

    Email creation
    Creating email

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from langchain import PromptTemplate
+from langchain.llms import Clarifai
 
 st.set_page_config(page_title="Generate your Email!!",page_icon=":📬:")
 
@@ -30,23 +32,105 @@ def main():
         st.write("## Email Preview")
         st.write(email_content)
 
+
+# function to generate email
 def generate_email(sender_name, recipient_name, subject, tone, attachments):
     # Generate the email content based on user inputs
-    email_template = f"Dear {recipient_name},\n\n"
+    # email_template = f"Dear {recipient_name},\n\n"
     
-    if tone == 'Formal':
-        email_template += "I hope this email finds you well. "
-    elif tone == 'Casual':
-        email_template += "Hey there, hope you're doing great! "
-    else:
-        email_template += "Hi, just wanted to drop you a quick note. "
+    # if tone == 'Formal':
+    #     email_template += "I hope this email finds you well. "
+    # elif tone == 'Casual':
+    #     email_template += "Hey there, hope you're doing great! "
+    # else:
+    #     email_template += "Hi, just wanted to drop you a quick note. "
     
-    email_template += f"I wanted to discuss the topic of '{subject}'. "
-    email_template += "Please find the attached files for your reference.\n\n"
+    # email_template += f"I wanted to discuss the topic of '{subject}'. "
+    # email_template += "Please find the attached files for your reference.\n\n"
     
-    email_template += f"Best regards,\n{sender_name}"
+    # email_template += f"Best regards,\n{sender_name}"
+
     
-    return email_template
+    ######################################################################################################
+    # In this section, we set the user authentication, user and app ID, model details, and the URL of 
+    # the text we want as an input. Change these strings to run your own example.
+    ######################################################################################################
+
+    # Your PAT (Personal Access Token) can be found in the portal under Authentification
+    PAT = '30cbbd7e47ba49c7a013f03c44f2b289'
+    # Specify the correct user_id/app_id pairings
+    # Since you're making inferences outside your app's scope
+    USER_ID = 'meta'
+    APP_ID = 'Llama-2'
+    # Change these to whatever model and text URL you want to use
+    MODEL_ID = 'llama2-13b-chat'
+    MODEL_VERSION_ID = '79a1af31aa8249a99602fc05687e8f40'
+    TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
+
+    ############################################################################
+    # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ############################################################################
+
+    from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+    from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+    from clarifai_grpc.grpc.api.status import status_code_pb2
+
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
+
+    metadata = (('authorization', 'Key ' + PAT),)
+
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+    post_model_outputs_response = stub.PostModelOutputs(
+        service_pb2.PostModelOutputsRequest(
+            user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
+            model_id=MODEL_ID,
+            version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        text=resources_pb2.Text(
+                            url=TEXT_FILE_URL
+                        )
+                    )
+                )
+            ]
+        ),
+        metadata=metadata
+    )
+    if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+        print(post_model_outputs_response.status)
+        raise Exception(f"Post model outputs failed, status: {post_model_outputs_response.status.description}")
+
+    # Since we have one input, one output will exist here
+    output = post_model_outputs_response.outputs[0]
+
+    print("Completion:\n")
+    print(output.data.text.raw)
+
+
+    # template for building the prompt
+    template = """Generate an email from {sender_name} to {recipient_name} with the following details:\nSubject: {subject}\nTone: {tone}"""
+    
+    if attachments:
+        attachment_names = ", ".join([attachment.name for attachment in attachments])
+        prompt += f"\nAttachments: {attachment_names}"
+
+    # creating the final prompt
+    prompt=PromptTemplate(
+        input_variables=["sender_name","recipient_name","subject","tone"],
+        template=template,)
+    
+    llm=Clarifai(pat='e620301e1ebe4aa5ba634bcc668f8274',user_id='meta',app_id='Llama-2',model_id='llama2-13b-chat')
+
+    # generating response using LLM
+    response=llm(prompt.format(subject=subject,sender_name=sender_name,recipient_name=recipient_name,tone=tone))
+    print(response)
+
+    return response
+    
+    # return email_template
 
 if __name__ == "__main__":
     main()
